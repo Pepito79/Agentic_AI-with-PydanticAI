428c69d768285fae14715b06d0ae88d9b872447d
pepito <saadguessous2023@gmail.com>
2025-01-22 08:51:59 +0100

    1st step : AI scrapper done

--- scrapper.py
+++ scrapper.py
@@ -3,33 +3,25 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import random
+import json
+from dotenv import load_dotenv
+import os
 
-def scrapper():
-   
-
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-    }
-    liens = []
-    for i in range(5):
-         
-         url_base = "https://html.duckduckgo.com/html/?q=kylian+mbappe&s={}"
-         url=url_base.format(i*10)
-         response = requests.get(url, headers=headers, allow_redirects=True)
-         print("Status code:", response.status_code)
-         if response.status_code != 200:
-              print("Erreur d'accès au serveur")
-         else:
-            soup = BeautifulSoup(response.text, 'html.parser')
-            resultats = soup.find_all('a', {'class': 'result__a'})
-            for resultat in resultats:
-                link = resultat.get('href')
-                liens.append(link)
-    time.sleep(random.uniform(2, 4))
-
+load_dotenv()
+api=os.getenv('SCRAPPER_API_KEY')
+def scrapper_liens(query: str, max_liens: int):
+    liens=[]
+    payload = {'api_key': api, 'query': query, 'country_code':'fr' , 'tld':'com','num':max_liens,'time_period':'1D'}
+    r = requests.get('https://api.scraperapi.com/structured/google/news', params=payload)
+    response = r.text
+    response_dict=json.loads(response)
+    for articles in response_dict['articles']:
+        liens.append(articles['link'])
     return liens
 
-
-
 if __name__ == "__main__":
-    print(scrapper())
+    liens = scrapper_liens("Bitcoin")
+    print("Liens extraits :", liens)
+
+
+
